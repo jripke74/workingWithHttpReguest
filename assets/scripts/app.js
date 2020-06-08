109e9a13b259682a3273d17c2563a3a418076f5c
jripke74 <jripke74@gmail.com>
2020-06-08 14:13:59 -0700

    add createPost methond

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -14,13 +14,17 @@ function sendHttpRequest(method, url) {
       // const listOfPosts = JSON.parse(xhr.response);
     };
 
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
+
   return promise;
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+  const responseData = await sendHttpRequest(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
   const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
@@ -30,4 +34,15 @@ async function fetchPosts() {
   }
 }
 
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId
+  };
+  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+}
+
 fetchPosts();
+createPost('DUMMY', 'A dummy psot!');
